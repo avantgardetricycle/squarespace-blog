eb3dc02801cce54a9f2f2276809344df290fc19b
Scott Duane <goodworkssoftware@gmail.com>
2026-02-22 21:59:06 -0800

    fix seed script

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -2,13 +2,12 @@ import 'dotenv/config'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../src/generated/prisma/client.js'
+import { getDatabaseUrl, getSslConfig } from '../src/lib/db-connection.js'
 
-const connectionString = process.env.DATABASE_URL
-if (!connectionString) {
-  throw new Error('DATABASE_URL is required')
-}
-
-const pool = new Pool({ connectionString })
+const pool = new Pool({
+  connectionString: getDatabaseUrl(),
+  ssl: getSslConfig()
+})
 const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({ adapter })
 
