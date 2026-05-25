224a5754c2a49b22f4ad64944c005c000b54d85e
Scott Duane <goodworkssoftware@gmail.com>
2026-05-25 11:50:50 -0700

    remove unused import

--- client/src/app/pages/Configure.tsx
+++ client/src/app/pages/Configure.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback, useMemo, useRef } from "react";
-import { useSearchParams, Link } from "react-router";
+import { useSearchParams } from "react-router";
 import {
   Copy,
   Monitor,
